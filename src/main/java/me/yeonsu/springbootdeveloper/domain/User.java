5b0effa6842ba93811ad5bcdd472d72be05b99bb
lilylemonoh <lilylemonoh@naver.com>
2023-10-22 22:26:41 +0900

    10장 OAuth2 글 수정, 삭제, 글쓴이 확인 로직 추가 전까지 작성

--- src/main/java/me/yeonsu/springbootdeveloper/domain/User.java
+++ src/main/java/me/yeonsu/springbootdeveloper/domain/User.java
@@ -29,10 +29,16 @@ public class User implements UserDetails {
     @Column(name = "password", nullable = false)
     private String password;
 
+    // 사용자 이름
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
+
     @Builder
-    public User(String email, String password, String auth) {
+    public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
     }
 
 
@@ -70,4 +76,14 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+
+    // 사용자 이름 변경
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
+    }
+
+
 }
